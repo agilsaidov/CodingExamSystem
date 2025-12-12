2e5f87c5e507d065fe8f5ce55c5262d86d40b60a
Aqil Səidov <aqilasa638@gmail.com>
2025-12-12 17:06:33 +0400

    JwtService implemented

---
+++ src/main/java/com/project/judge/security/JwtService.java
@@ -0,0 +1,71 @@
+package com.project.judge.security;
+
+import com.project.judge.model.AppUser;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtService {
+
+    @Value("${JWT_SECRET}")
+    private String jwtSecret;
+
+    public String generateToken(AppUser user) {
+        Map<String, Object> claims = new HashMap<>();
+
+        claims.put("username", user.getUserName());
+        claims.put("name", user.getFullName());
+        claims.put("role", user.getRole());
+
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(user.getUserId())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + (3 * 60 * 60 * 1000)))
+                .and()
+                .signWith(getSecretKey())
+                .compact();
+    }
+
+
+    public boolean validateToken(String token) {
+        Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token);
+        return true;
+    }
+
+    public String getRoleFromToken(String token) {
+        Claims claims = getClaims(token);
+        return claims.get("role", String.class);
+    }
+
+    public String getIdFromToken(String token) {
+        Claims claims = getClaims(token);
+        return claims.getSubject();
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
+}
